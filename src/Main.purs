532767665b6ea3d25c9652a7068c159fbb80ba1f
Tomasz Rybarczyk <paluho@gmail.com>
2016-09-21 15:53:58 +0200

    Use liftEff

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,21 @@
 module Main where
 
 import Prelude
-import Control.Monad.Aff (liftEff')
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Exception (EXCEPTION)
 import Pux (onlyEffects, Update)
+import Signal.Channel (CHANNEL)
 
 data State = State
 data Action = Action
 
-push :: forall eff. Eff eff Unit
+push :: forall eff. Eff (channel :: CHANNEL, err :: EXCEPTION | eff) Unit
 push = pure unit
 
 update :: forall eff. Update State (Action) eff
 update Action s =
-  onlyEffects s [ liftEff' push >>= (const $ pure Action) ]
+  onlyEffects s [ liftEff push >>= (const $ pure Action) ]
 
 main :: forall eff. Eff eff Unit
 main = pure unit
